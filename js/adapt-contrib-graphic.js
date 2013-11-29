6d35ee892082b0924099d63c291beb3a72a964e6
brian-learningpool <brian@learningpool.com>
2013-11-29 16:46:35 +0000

    Work in progress
    Image 'src' attribute is updated depending on screen width.  Currently not
    working 100%.

--- js/adapt-contrib-graphic.js
+++ js/adapt-contrib-graphic.js
@@ -1,13 +1,49 @@
-define(["coreViews/componentView", "coreJS/adapt"], function(ComponentView, Adapt) {
+define(function(require) {
+
+	var ComponentView = require("coreViews/componentView");
+	var Adapt = require("coreJS/adapt");
 
     var Graphic = ComponentView.extend({
-        
+
+    	init: function() {
+    		// TODO -- Will the next line always be required?
+    		this.constructor.template = this.model.get('_component');
+			this.listenTo(Adapt, 'device:resize', this.resizeImage);
+
+			// this.$('.widget').imageready(function(){
+			// 	this.model.set('ready', true);
+			// });
+  		},
         postRender: function() {
             console.log("rendering");
             this.setReadyStatus();
             this.setCompletionStatus();
+        },
+        resizeImage: function(width) {
+        	var attribute = '';
+        	switch (true) {
+        		case (width < 760):
+        			console.log('small');
+        			attribute = 'data-small';
+        			break;
+    			case (width >= 760 && width < 960):
+    				console.log('medium');
+    				attribute = 'data-medium';
+    				break;
+				case (width >= 960):
+					console.log('large');
+					attribute = 'data-large';
+					break;
+        	}
+
+        	if (attribute.length != 0) {
+        		// Swap in one image for another
+				var src = this.$('.widget img').attr(attribute);
+        		this.$('.widget img').attr('src', src);
+        	}
+
+        	
         }
-        
     });
     
     Adapt.register("graphic", Graphic);
