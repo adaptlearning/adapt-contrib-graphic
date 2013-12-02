33fb8817a46a8d0c804a06831db1b2c0eb63635e
brian-learningpool <brian@learningpool.com>
2013-12-02 11:07:59 +0000

    Implentation of handler for device:change event
    The appropriate image is now also set on the page load.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 adapt-contrib-graphic
 =====================
 
-A graphic component for adapt that displays different image resolutions based upon device width
+A component for the Adapt Framework that dynamically displays image of different resolutions based upon device width.  
+
+Three image widths from the _AdaptBuilder.screenSize_ object are handled:
+* small
+* medium
+* large
+
+In order to render images, these three corresponding data attributes in the JSON must be set to the source of the various images.
+* _data-small_ 
+* _data-medium_
+* _data-large_
+
+When the viewport size changes, these attributes will be swapped in for the _src_ tag.
--- js/adapt-contrib-graphic.js
+++ js/adapt-contrib-graphic.js
@@ -8,41 +8,16 @@ define(function(require) {
     	init: function() {
     		// TODO -- Will the next line always be required?
     		this.constructor.template = this.model.get('_component');
-			this.listenTo(Adapt, 'device:resize', this.resizeImage);
-
-			// this.$('.widget').imageready(function(){
-			// 	this.model.set('ready', true);
-			// });
+			this.listenTo(Adapt, 'device:changed', this.resizeImage);
   		},
         postRender: function() {
-            console.log("rendering");
+            this.resizeImage(Adapt.screenSize);
             this.setReadyStatus();
             this.setCompletionStatus();
         },
         resizeImage: function(width) {
-        	var attribute = '';
-        	switch (true) {
-        		case (width < 760):
-        			console.log('small');
-        			attribute = 'data-small';
-        			break;
-    			case (width >= 760 && width < 960):
-    				console.log('medium');
-    				attribute = 'data-medium';
-    				break;
-				case (width >= 960):
-					console.log('large');
-					attribute = 'data-large';
-					break;
-        	}
-
-        	if (attribute.length != 0) {
-        		// Swap in one image for another
-				var src = this.$('.widget img').attr(attribute);
-        		this.$('.widget img').attr('src', src);
-        	}
-
-        	
+    		var src = this.$('.widget img').attr('data-' + width);
+        	this.$('.widget img').attr('src', src);
         }
     });
     
