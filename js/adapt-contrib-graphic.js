7e98f3ece1bf8ebc9017e22321a588cbaae068d7
brian-learningpool <brian@learningpool.com>
2013-12-16 11:32:16 +0000

    Re-work to call setReadyStatus()
    Work in progress

--- js/adapt-contrib-graphic.js
+++ js/adapt-contrib-graphic.js
@@ -1,25 +1,24 @@
 define(function(require) {
 
-	var ComponentView = require("coreViews/componentView");
-	var Adapt = require("coreJS/adapt");
+    var ComponentView = require("coreViews/componentView");
+    var Adapt = require("coreJS/adapt");
 
     var Graphic = ComponentView.extend({
 
-    	init: function() {
-            this.$('.widget').imageready(function(){
-                this.model.set('ready', true);
-            });
+        init: function() {
+            this.$('.widget').imageready(_.bind(function() {
+                this.setReadyStatus();
+            }, this));
 
-			this.listenTo(Adapt, 'device:changed', this.resizeImage);
-  		},
+            this.listenTo(Adapt, 'device:changed', this.resizeImage);
+        },
         postRender: function() {
             this.resizeImage(Adapt.device.screenSize);
-            this.setReadyStatus();
             this.setCompletionStatus();
         },
         resizeImage: function(width) {
-    		var src = this.$('.widget img').attr('data-' + width);
-        	this.$('.widget img').attr('src', src);
+            var src = this.$('.widget img').attr('data-' + width);
+            this.$('.widget img').attr('src', src);
         }
     });
     
