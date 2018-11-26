4490470bac06810874a41d35d86b56df05fa76d8
Matt Leathes <mleathes+github@gmail.com>
2018-11-26 10:17:13 +0000

    switch over to the new ComponentView inview functionality (#69)
    * use new ComponentView inview functionality
    
    * code tidy, remove pointless comments

--- js/adapt-contrib-graphic.js
+++ js/adapt-contrib-graphic.js
@@ -1,14 +1,13 @@
-define(function(require) {
-
-    var ComponentView = require('coreViews/componentView');
-    var Adapt = require('coreJS/adapt');
+define([
+    'core/js/adapt',
+    'core/js/views/componentView'
+], function(Adapt, ComponentView) {
 
     var Graphic = ComponentView.extend({
 
         preRender: function() {
             this.listenTo(Adapt, 'device:changed', this.resizeImage);
 
-            // Checks to see if the graphic should be reset on revisit
             this.checkIfResetOnRevisit();
         },
 
@@ -16,40 +15,12 @@ define(function(require) {
             this.resizeImage(Adapt.device.screenSize, true);
         },
 
-        // Used to check if the graphic should reset on revisit
         checkIfResetOnRevisit: function() {
             var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-            // If reset is enabled set defaults
             if (isResetOnRevisit) {
                 this.model.reset(isResetOnRevisit);
             }
-        },
-
-        inview: function(event, visible, visiblePartX, visiblePartY) {
-            if (visible) {
-                if (visiblePartY === 'top') {
-                    this._isVisibleTop = true;
-                } else if (visiblePartY === 'bottom') {
-                    this._isVisibleBottom = true;
-                } else {
-                    this._isVisibleTop = true;
-                    this._isVisibleBottom = true;
-                }
-
-                if (this._isVisibleTop && this._isVisibleBottom) {
-                    this.$('.component-widget').off('inview');
-                    this.setCompletionStatus();
-                }
-
-            }
-        },
-
-        remove: function() {
-          // Remove any 'inview' listener attached.
-          this.$('.component-widget').off('inview');
-
-          ComponentView.prototype.remove.apply(this, arguments);
         },
 
         resizeImage: function(width, setupInView) {
@@ -57,19 +28,16 @@ define(function(require) {
             var imageSrc = (this.model.get('_graphic')) ? this.model.get('_graphic')[imageWidth] : '';
             this.$('.graphic-widget img').attr('src', imageSrc);
 
-            this.$('.graphic-widget').imageready(_.bind(function() {
+            this.$('.graphic-widget').imageready(function() {
                 this.setReadyStatus();
 
                 if (setupInView) {
-                    // Bind 'inview' once the image is ready.
-                    this.$('.component-widget').on('inview', _.bind(this.inview, this));
+                    this.setupInviewCompletion('.component-widget');
                 }
-            }, this));
+            }.bind(this));
         }
     });
 
-    Adapt.register('graphic', Graphic);
-
-    return Graphic;
+    return Adapt.register('graphic', Graphic);
 
 });
