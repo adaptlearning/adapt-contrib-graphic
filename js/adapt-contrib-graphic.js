d0fbb2982cff0b9f76e07ee046efa5c19551d032
Guy Willis <guy.willis@kineo.com>
2018-12-13 13:30:40 +0000

    Converted tabs to spaces, indentation amends

--- js/adapt-contrib-graphic.js
+++ js/adapt-contrib-graphic.js
@@ -1,43 +1,45 @@
 define([
-    'core/js/adapt',
-    'core/js/views/componentView'
+  'core/js/adapt',
+  'core/js/views/componentView'
 ], function(Adapt, ComponentView) {
 
-    var Graphic = ComponentView.extend({
+  var Graphic = ComponentView.extend({
 
-        preRender: function() {
-            this.listenTo(Adapt, 'device:changed', this.resizeImage);
+    preRender: function() {
+      this.listenTo(Adapt, 'device:changed', this.resizeImage);
 
-            this.checkIfResetOnRevisit();
-        },
+      this.checkIfResetOnRevisit();
+    },
 
-        postRender: function() {
-            this.resizeImage(Adapt.device.screenSize, true);
-        },
+    postRender: function() {
+      this.resizeImage(Adapt.device.screenSize, true);
+    },
 
-        checkIfResetOnRevisit: function() {
-            var isResetOnRevisit = this.model.get('_isResetOnRevisit');
+    checkIfResetOnRevisit: function() {
+      var isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-            if (isResetOnRevisit) {
-                this.model.reset(isResetOnRevisit);
-            }
-        },
+      if (isResetOnRevisit) {
+        this.model.reset(isResetOnRevisit);
+      }
+    },
 
-        resizeImage: function(width, setupInView) {
-            var imageWidth = width === 'medium' ? 'small' : width;
-            var imageSrc = (this.model.get('_graphic')) ? this.model.get('_graphic')[imageWidth] : '';
-            this.$('.graphic-widget img').attr('src', imageSrc);
+    resizeImage: function(width, setupInView) {
+      var imageWidth = width === 'medium' ? 'small' : width;
+      var imageSrc = (this.model.get('_graphic')) ? this.model.get('_graphic')[imageWidth] : '';
+      this.$('.graphic-widget img').attr('src', imageSrc);
 
-            this.$('.graphic-widget').imageready(function() {
-                this.setReadyStatus();
+      this.$('.graphic-widget').imageready(function() {
+        this.setReadyStatus();
 
-                if (setupInView) {
-                    this.setupInviewCompletion('.component-widget');
-                }
-            }.bind(this));
+        if (setupInView) {
+          this.setupInviewCompletion('.component-widget');
         }
-    });
 
-    return Adapt.register('graphic', Graphic);
+      }.bind(this));
+    }
+
+  });
+
+  return Adapt.register('graphic', Graphic);
 
 });
