27ec36e33a22e8aacbc0b5da7abb4b7b8a0a04b2
Guy Willis <guy.willis@kineo.com>
2021-10-19 17:20:58 +0100

    Converted to support new jsx version

--- js/GraphicView.js
+++ js/GraphicView.js
@@ -2,11 +2,37 @@ import ComponentView from 'core/js/views/componentView';
 
 class GraphicView extends ComponentView {
 
+  events() {
+    return {
+      'click .js-graphic-link': 'onClick'
+    };
+  }
+
   postRender() {
     this.$('.graphic__widget').imageready(() => {
       this.setReadyStatus();
       this.setupInviewCompletion('.graphic__widget');
     });
+  }
+
+  onClick(event) {
+    if (event) event.preventDefault();
+
+    const item = this.model.get('_graphic');
+    const url = item._url;
+    const target = item._target || '_blank';
+
+    switch (target) {
+      case '_self':
+        if (url.substr(0, 1) === '#') {
+          Backbone.history.navigate(url, { trigger: true });
+          return;
+        }
+        window.location.href = url;
+        break;
+      default:
+        window.open(url, target);
+    }
   }
 
 }
