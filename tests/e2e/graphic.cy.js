5958a52319c761a1c6a70498f60ba5700fd3d4a8
Lemmy Adams <lemmy.adams@learningpool.com>
2024-02-05 16:58:34 +0000

    Comments

--- tests/e2e/graphic.cy.js
+++ tests/e2e/graphic.cy.js
@@ -7,26 +7,23 @@ describe('Graphic', function () {
   it('should display the graphic component', function () {
     const graphicComponents = this.data.components.filter((component) => component._component === 'graphic')
     graphicComponents.forEach((graphicComponent) => {
-      let waited = false
       cy.visit(`/#/preview/${graphicComponent._id}`);
       const bodyWithoutHtml = graphicComponent.body.replace(/<[^>]*>/g, '');
-
+      
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      let waited = false
       function waitOneSecond() {
-        // return a promise that resolves after 1 second
         return new Cypress.Promise((resolve, reject) => {
           setTimeout(() => {
-            // set waited to true
             waited = true
     
-            // resolve with 'foo' string
             resolve()
           }, 1000)
         })
       }
 
       cy.wrap(null).then(() => {
-        // return a promise to cy.then() that
-        // is awaited until it resolves
         return waitOneSecond().then(() => {
           cy.testContainsOrNotExists('.graphic__title', graphicComponent.displayTitle)
           cy.testContainsOrNotExists('.graphic__body', bodyWithoutHtml)
