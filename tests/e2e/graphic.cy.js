95596f1b7ad566965a8bbe2e81cb7d3a91f9f629
Lemmy Adams <lemmy.adams@learningpool.com>
2024-02-07 11:02:55 +0000

    Changed back to wait

--- tests/e2e/graphic.cy.js
+++ tests/e2e/graphic.cy.js
@@ -10,31 +10,17 @@ describe('Graphic', function () {
       cy.visit(`/#/preview/${graphicComponent._id}`);
       const bodyWithoutHtml = graphicComponent.body.replace(/<[^>]*>/g, '');
       
+      cy.testContainsOrNotExists('.graphic__title', graphicComponent.displayTitle)
+      cy.testContainsOrNotExists('.graphic__body', bodyWithoutHtml)
+      if(graphicComponent._graphic.large) {
+        cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.large)
+      } else if(graphicComponent._graphic.src) {
+        cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.src)
+      }
+      
       // Make sure the current component is tested before moving to the next one
       // Custom cypress tests are async so we need to wait for them to pass first
-      let waited = false
-      function waitOneSecond() {
-        return new Cypress.Promise((resolve, reject) => {
-          setTimeout(() => {
-            waited = true
-    
-            resolve()
-          }, 1000)
-        })
-      }
-
-      cy.wrap(null).then(() => {
-        return waitOneSecond().then(() => {
-          cy.testContainsOrNotExists('.graphic__title', graphicComponent.displayTitle)
-          cy.testContainsOrNotExists('.graphic__body', bodyWithoutHtml)
-          if(graphicComponent._graphic.large) {
-            cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.large)
-          } else if(graphicComponent._graphic.src) {
-            cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.src)
-          }
-          expect(waited).to.be.true
-        })
-      })
+      cy.wait(1000)
     });
   });
 });
