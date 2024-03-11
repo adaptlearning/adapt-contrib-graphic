7f3f900a507324b3ac231fabf030f1c97574f413
Lemmy Adams <103187526+lemmyadams@users.noreply.github.com>
2024-03-11 11:33:06 +0000

    Fix: e2e tests on graphic component (fixes #95) (#97)
    * Added graphic tests
    
    * Improved graphic tests for new system [wip]
    
    * Updated to use preview pages
    
    * Comments
    
    * Changed back to wait
    
    * removed visit /

---
+++ tests/e2e/graphic.cy.js
@@ -0,0 +1,25 @@
+describe('Graphic', function () {
+  beforeEach(function () {
+    cy.getData()
+  });
+
+  it('should display the graphic component', function () {
+    const graphicComponents = this.data.components.filter((component) => component._component === 'graphic')
+    graphicComponents.forEach((graphicComponent) => {
+      cy.visit(`/#/preview/${graphicComponent._id}`);
+      const bodyWithoutHtml = graphicComponent.body.replace(/<[^>]*>/g, '');
+      
+      cy.testContainsOrNotExists('.graphic__title', graphicComponent.displayTitle)
+      cy.testContainsOrNotExists('.graphic__body', bodyWithoutHtml)
+      if(graphicComponent._graphic.large) {
+        cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.large)
+      } else if(graphicComponent._graphic.src) {
+        cy.get('.graphic__image').should('have.attr', 'src', graphicComponent._graphic.src)
+      }
+      
+      // Make sure the current component is tested before moving to the next one
+      // Custom cypress tests are async so we need to wait for them to pass first
+      cy.wait(1000)
+    });
+  });
+});
